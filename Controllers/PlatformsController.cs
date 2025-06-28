65ecb23028703fe71317502407b23ffb1fe2fd50
LordSexyWolf <joshuasalinashernandez@gmail.com>
2025-06-28 08:49:43 -0600

    Controller added and tested in Insomnia

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using GamePlatformService.Data;
 using GamePlatformService.Dtos;
+using GamePlatformService.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -25,6 +26,29 @@ namespace GamePlatformService.Controllers
             var platformItems = _repository.GetAllPlatforms();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDtos>> (platformItems));
+        }
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDtos> GetPlatformById(int id)
+        {
+            var platformItems = _repository.GetPlatformById(id);
+
+            if(platformItems != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDtos>(platformItems));
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDtos> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDtos>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
         }
     }
 }
